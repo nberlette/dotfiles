6c7664dc9bb68291e0a4094e6ee8cf767b2655eb
Nicholas Berlette <nick@berlette.com>
2022-04-14 14:10:43 -0700

    💩 update .bash_profile and install.sh

--- .bash_profile
+++ .bash_profile
@@ -2,20 +2,101 @@
 shell="$(which bash)"
 export SHELL="$shell"
 export DOTFILES_PREFIX="${DOTFILES_PREFIX:-"$HOME/.dotfiles"}"
-[ -r "${DOTFILES_PREFIX:-.}/.path" ] && source "${DOTFILES_PREFIX:-.}/.path"
+
+[ -r ~/.path ] && source ~/.path;
+
 # import all vars from .env + .extra into current environment
-srx "${DOTFILES_PREFIX:-.}"/.{env,extra}
+srx ~/.{env,extra}
+
 # include our core bash environment
-src "${DOTFILES_PREFIX:-.}"/.{exports,functions,bash_aliases}
+src ~/.{exports,functions,bash_aliases,profile}
+
 # ruby version manager
-src "${DOTFILES_PREFIX:-.}/.rvm/scripts/rvm"
+src ~/.rvm/scripts/rvm
+
 # TODO: move this to an install script so it's always ready on runtime
-function __prompt() {
-	if which starship >&/dev/null; then
-		eval "$(starship completions bash 2> /dev/null)"
-		eval "$(starship init bash 2> /dev/null)"
-	else
-		brew install starship --quiet && __prompt
-	fi
+function get_var() {
+  eval 'printf "%s\n" "${'"$1"'}"'
 }
-__prompt && unset -v __prompt
+
+function set_var() {
+  eval "$1=\"\$2\""
+}
+
+function dedupe_path() {
+  local pathvar_name pathvar_value deduped_path
+  pathvar_name="${1:-PATH}"
+  pathvar_value="$(get_var "$pathvar_name")"
+  deduped_path="$(perl -e 'print join(":",grep { not $seen{$_}++ } split(/:/, $ARGV[0]))' "$pathvar_value")"
+  set_var "$pathvar_name" "$deduped_path"
+}
+
+function get_json() {
+  local path file
+  file="${1:-"/workspace/.vscode-remote/settings.json"}"
+  path="${2:-git.enableCommitSigning}"
+  jq '.["'$path'"]' "$file" 2>/dev/null || return $?
+}
+
+function set_json() {
+  local path value file
+  file="${1:-"/workspace/.vscode-remote/settings.json"}"
+  path="${2:-git.enableCommitSigning}"
+  value="${3:-true}"
+  jq '.["'$path'"]=$value | .' "$file" 2>/dev/null || return $?
+}
+
+function __gpg_gitconfig () {
+  [ -n "${GPG_KEY_ID-}" ] && git config --global user.signingkey "${GPG_KEY_ID-}"
+  git config --global commit.gpgsign "true"
+  git config --global tag.gpgsign "true"
+}
+
+function __gpg_vscode () {
+  local VSCODE SETTINGS_JSON
+  VSCODE="/workspace/.vscode-remote"
+  [ -d "$VSCODE" ] || mkdir -p "$VSCODE"
+  SETTINGS_JSON="$VSCODE/settings.json"
+  [ -f "$SETTINGS_JSON" ] || echo "{}" >"$SETTINGS_JSON"
+  if [[ "$(get_json "$SETTINGS_JSON" git.enableCommitSigning)" != 'true' ]]; then
+    set_json "$SETTINGS_JSON" 'git.enableCommitSigning' 'true'
+  fi
+}
+
+function __gpg_unlock () {
+  local PINENTRY_CONF GPG_CONF
+  PINENTRY_CONF='pinentry-mode loopback'
+  GPG_CONF="$HOME/.gnupg/gpg.conf"
+  unset -v GPG_CONFIGURED
+  touch "$GPG_CONF"
+  if ! grep -q "$PINENTRY_CONF" "$GPG_CONF" >/dev/null 2>&1; then
+    echo "$PINENTRY_CONF" >>"$GPG_CONF"
+  fi
+  gpg --batch --import <(echo "${GPG_KEY-}" | base64 -d) >&/dev/null
+
+  __gpg_gitconfig
+  __gpg_vscode
+
+  gpgconf --kill gpg-agent
+  gpg-connect-agent reloadagent /bye >&/dev/null
+  export GPG_CONFIGURED=1
+}
+
+export GPG_TTY=$(tty)
+[ -n "${GPG_KEY-}" ] && __gpg_unlock 2>/dev/null
+
+# starship shell prompt with fallback
+__prompt () {
+  if ! which starship >&/dev/null; then
+    # its horribly hacky, but I'm short on time and it works for now. (I hope)
+    brew install starship --quiet && __prompt 2>/dev/null;
+  fi
+
+  eval "$(starship completions bash 2>/dev/null)"
+  eval "$(starship init bash 2>/dev/null)"
+};
+# instantiate and cleanup afterwards
+__prompt && unset -f __prompt || {
+  PROMPT_COMMAND+=' __git_ps1 "[\$?] '"${GIT_PS1_PREFIX-}"'" "'"${GIT_PS1_SUFFIX-}"'" "'"${GIT_PS1_FORMAT:- %s }"'"'
+  export PROMPT_COMMAND
+};
--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ OSYS="$(uname | tr '[:upper:]' '[:lower:]')"
 [ -z "$CI" ] && verbosity="--quiet" || verbosity="--verbose"
 [ -z "$TERM" ] && export TERM=xterm
 
+STEP_NUM=1
+STEP_TOTAL=3
+
 function curdir() {
   echo -n "$(readlink $(test -z "$CI" && echo -n "-f" || echo -n "-n") "$(dirname -- "${BASH_SOURCE[0]}")" 2>/dev/null || echo -n "$(cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)")"
 }
@@ -51,6 +54,7 @@ function setup_home() {
 
 # install homebrew (if needed) and some commonly-used global packages. the bare minimum.
 function setup_brew() {
+  print_banner step $'Installing/upgrading \033[1;4;35mhomebrew\033[0;1m and required formulae...'
   # install homebrew if not already installed
   if ! which brew >&/dev/null; then
     curl -fsSL https://raw.github.com/Homebrew/install/HEAD/install.sh | bash -
@@ -59,7 +63,7 @@ function setup_brew() {
   # execute now just to be sure its available for us immediately
   eval "$(brew shellenv 2>/dev/null)"
 
-  brew install "${verbosity-}" --overwrite starship gh
+  brew install "${verbosity-}" --overwrite starship gh supabase/tap/supabase
 
   # don't install when we're in a gitpod environment
   # otherwise this step takes > 120 seconds and fails to install.
@@ -67,6 +71,7 @@ function setup_brew() {
   if [ -z "$CI" ]; then
     if [ -n "$SUPERVISOR_DOTFILE_REPO" ] || [ -n "$IS_DARWIN" ]; then
       which rvm >&/dev/null || command curl -fsSL https://get.rvm.io | bash -s stable --rails;
+
       # install some essentials
       brew install "${verbosity-}" --overwrite \
         gcc \
@@ -79,7 +84,6 @@ function setup_brew() {
         python \
         fontforge \
         jq \
-        supabase/tap/supabase \
         docker \
         fzf \
         neovim \
@@ -87,11 +91,11 @@ function setup_brew() {
         shellcheck \
         shfmt \
         pygments \
-        2>/dev/null
+      2>/dev/null
 
       # for developing jekyll sites (and other things for github pages, etc)
       if which gem >&/dev/null; then
-        sudo gem install jekyll builder jekyll-vite rouge
+        gem install jekyll bundler jekyll-vite rouge
       fi
 
       # for macOS
@@ -113,20 +117,21 @@ function setup_brew() {
           google-chrome google-chrome-canary firefox firefox-nightly
 
       else
-        brew install --overwrite gnupg gnupg2
+        brew install --overwrite gnupg gnupg2 xclip
       fi
     fi
   fi
 }
 
 function global_add() {
-  local pkg pkgs agent
+  local pkg pkgs agent i=1
   agent=$(which pnpm 2>/dev/null || which npm 2>/dev/null)
   pkgs=("$@")
-  "$agent" add -g "$@" >&/dev/null && {
+  eval "$("$agent" add -g "$@" >&/dev/null)" && {
     echo "Successfully installed $# global packages!"
     for pkg in "${pkgs[@]}"; do
-      echo "  - $pkg"
+      echo " ${i-}. $pkg "
+      ((i++))
     done
   }
 }
@@ -155,37 +160,74 @@ function setup_node() {
 
 # runs all the other scripts and cleans up after itself. geronimo!
 function main() {
-  clear
-  echo -e "\\n\\033[1mBeginning dotfiles install. This may take a while...\\033[0m"
-  echo -e "-------------------------------------------------------\\n"
-  echo "WORKDIR: $(curdir)"
+  function print_banner () {
+    local message divider divider_length i
+    case "${1-}" in
+      (step)
+        printf '\033[2m(%d / %d) \033[0;1m%s\033[0m' "$STEP_NUM" "$STEP_TOTAL" "${*:2}"
+      ;;
+      (*)
+          divider="" divider_char="-"
+          if [[ ${#1} == 1 && -n "$2" ]]; then
+            divider_char="${1:-"-"}"
+            message="${*:2}"
+          else
+            message="${*:-"Beginning dotfiles installation"}"
+          fi
+          for ((i=0;$i<${COLUMNS:-100};i++)); do
+            divider+="${divider_char:-"="}"
+          done
+          clear && printf '\n\033[1m %s \033[0m\n\033[2m%s\033[0m\n' "${message-}" "${divider-}"
+      ;;
+    esac
+  }
 
-  if [[ "$(uname -s)" == "Darwin" ]]; then
-    export PATH="$HOME/Library/pnpm:/usr/local/bin:/usr/local/sbin:/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
-  else
-    export PATH="$HOME/.local/share/pnpm:/usr/local/bin:/usr/local/sbin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
-  fi
+  function print_step_complete () {
+    local delay=3
+    if (($# > 0)); then
+      printf '\n\033[1;3;42m\033[38;2;255;255;255m %s \033[0;2;3m %s\n\n' "${1-}" "${*:2}"
+    else
+      # pause for a moment and clear the screen
+      sleep "${delay:-3}s" && clear
+      # display the completed step number and total number of steps
+      echo -e '\n\033[1;4m Completed step '$STEP_NUM' of '$STEP_TOTAL'.\033[0m\n\n'
+      # increment step number by 1
+      ((step++))
+      # pause and clear again before proceeding to next step
+      sleep "${delay:-3}s" && clear
+    fi
+  }
 
-  echo -e '1. \033[1mLinking \033[1;4;33mdotfiles\033[0;1m to \033[1;3;4;36mHOMEDIR\033[0m...'
-  setup_home && unset -f setup_home
-  sleep 1 && clear && echo -e '\033[1;3;4mFinished step 1 of 3.\033[0m'
+  print_banner "Beginning dotfiles installation. This may take a few minutes."
+  printf ' \033[1;4m%s\033[0m: %s ' 'Working Dir' "$(curdir)"
 
-  echo -e '2. \033[1mInstalling \033[1;4;31mPNPM\033[0;1m and \033[1;4;32mNode.js\033[0;1;2m (latest LTS release)\033[0m'
-  setup_node && unset -f setup_node
-  sleep 1 && clear && echo -e '\033[1;3;4mFinished step 2 of 3.\033[0m'
+  print_banner step $'Linking \033[1;4;33mdotfiles\033[0;1m to \033[1;3;4;36m/home/gitpod'
+  setup_home >> ~/.dotfiles/.install.log \
+    && print_step_complete
 
-  echo -e '3. \033[1mInstalling or updating \033[1;4;35mhomebrew\033[0;1m, and some of the bare-minimum tooling...\033[0m'
-  setup_brew && unset -f setup_brew
-  sleep 1 && clear && echo -e '\033[1;3;4mFinished step 3 of 3.\033[0m'
-  sleep 2 && clear && echo -e '\033[1;3;4;42m\033[38;2;255;255;255m COMPLETE! \033[0m Restarting bash... happy coding!'
-  sleep 2 && clear && unset -v OSYS IS_DARWIN curdir verbosity
+  print_banner step $'Installing/upgrading \033[1;4;31mPNPM\033[0;1m and \033[4;32mNode LTS\033[0;1;2m (latest)'
+  setup_node >> ~/.dotfiles/.install.log \
+    && print_step_complete
+
+  print_banner step $'Installing/upgrading \033[1;4;35mhomebrew\033[0;1m and required formulae...'
+  setup_brew >> ~/.dotfiles/.install.log \
+    && print_step_complete
+
+  print_step_complete 'COMPLETE!' 'Restarting shell environment...'
+  sleep 4s && clear
+
   # shellcheck source=/dev/null
   source /home/gitpod/.bashrc 2>/dev/null || return $?
   return 0
 }
 
+function cleanup_env () {
+  unset -v OSYS IS_DARWIN STEP_NUM STEP_TOTAL verbosity
+  unset -f main setup_home setup_node setup_brew print_step_complete curdir
+}
+
 # run it and clean up after!
-main && unset -f main && exit 0
+(main && cleanup_env && unset -f cleanup_env) || exit $?
 
 # still here? throw codes bro
-exit $?
+exit 1

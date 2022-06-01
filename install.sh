b7befa956897a6c52cadabe5d82871fabbd1c7b1
Nicholas Berlette <nick@berlette.com>
2022-05-31 19:35:15 -0700

    Refactor install.sh, add .Brewfiles and .osx, delete .profile

--- install.sh
+++ install.sh
@@ -59,6 +59,33 @@ if ! hash curdir &> /dev/null; then
   }
 fi
 
+# $OSTYPE variable (linux-gnu, darwin, etc)
+[ -z "${OSTYPE:+x}" ] && OSTYPE=$(ostype)
+
+# $IS_DARWIN=1 if on a Mac
+[[ $OSTYPE == [Dd]arwin* ]] && IS_DARWIN=1 || IS_DARWIN="";
+
+# IS_INTERACTIVE=1 if interactive, 0 otherwise (duh)
+# (like in CI/CD, or Codespaces/Gitpod autoinstall during prebuilds)
+declare -g -r -x IS_INTERACTIVE="$(is_interactive 2>&1 && echo -n 1)"
+
+export TZ='America/Los_Angeles'
+declare -g -r -x DOTFILES_PREFIX="${DOTFILES_PREFIX:-"$HOME/.dotfiles"}"
+declare -g -r -x DOTFILES_LOGPATH="$DOTFILES_PREFIX/_installs/$(date +%F)-$(date +%s)"
+declare -g -r -x DOTFILES_LOG="${DOTFILES_LOGPATH-}/install.log"
+declare -g -r -x DOTFILES_BACKUP_PATH="${DOTFILES_LOGPATH}/.backup"
+[ -d "$DOTFILES_LOGPATH" ] || mkdir -p "$DOTFILES_LOGPATH" &> /dev/null
+[ -d "$DOTFILES_BACKUP_PATH" ] || mkdir -p "$DOTFILES_BACKUP_PATH" &> /dev/null
+command touch "$DOTFILES_LOG" &> /dev/null
+
+DOTFILES_CORE="$(curdir 2> /dev/null || echo -n "$DOTFILES_PREFIX")/.bashrc.d/core.sh"
+
+# shellcheck source=/dev/null
+[ -r "$DOTFILES_CORE" ] && . "$DOTFILES_CORE"
+
+cd "$(curdir)" 2> /dev/null || exit 1
+
+
 # check for global_add
 if ! hash global_add &> /dev/null; then
   # installs all arguments as global packages
@@ -82,39 +109,6 @@ if ! hash global_add &> /dev/null; then
 
 fi
 
-function setup_env() {
-  declare -l DOTFILES_CORE
-  declare -g IS_INTERACTIVE DOTFILES_PREFIX
-  declare -g -r DOTFILES_LOG DOTFILES_LOGPATH DOTFILES_BACKUP_PATH
-
-  # $OSTYPE variable (linux-gnu, darwin, etc)
-  [ -z "${OSTYPE:+x}" ] && OSTYPE=$(ostype)
-
-  # $IS_DARWIN=1 if on a Mac
-  [[ $OSTYPE == [Dd]arwin* ]] && IS_DARWIN=1 || IS_DARWIN=0
-
-  # IS_INTERACTIVE=1 if interactive, 0 otherwise (duh)
-  # (like in CI/CD, or Codespaces/Gitpod autoinstall during prebuilds)
-  IS_INTERACTIVE="$(is_interactive 2>&1)"
-
-  export TZ='America/Los_Angeles'
-  export DOTFILES_PREFIX="${DOTFILES_PREFIX:-"$HOME/.dotfiles"}"
-
-  export DOTFILES_LOGPATH="$DOTFILES_PREFIX/_installs/$(date +%F)-$(date +%s)"
-  export DOTFILES_LOG="${DOTFILES_LOGPATH-}/install.log"
-  export DOTFILES_BACKUP_PATH="${DOTFILES_LOGPATH}/.backup"
-  [ -d "$DOTFILES_LOGPATH" ] || mkdir -p "$DOTFILES_LOGPATH" &> /dev/null
-  [ -d "$DOTFILES_BACKUP_PATH" ] || mkdir -p "$DOTFILES_BACKUP_PATH" &> /dev/null
-  command touch "$DOTFILES_LOG" &> /dev/null
-
-  DOTFILES_CORE="$(curdir 2> /dev/null || echo -n "$DOTFILES_PREFIX")/.bashrc.d/core.sh"
-
-  # shellcheck source=/dev/null
-  [ -r "$DOTFILES_CORE" ] && . "$DOTFILES_CORE"
-
-  cd "$(curdir)" 2> /dev/null || exit 1
-}
-
 function print_banner() {
   local message divider i
   case "${1-}" in
@@ -137,6 +131,7 @@ function print_banner() {
   esac
 }
 
+# shellcheck disable=SC2120
 function print_step_complete() {
   if (($# > 0)); then
     printf '\n\033[1;48;2;40;60;66;38;2;240;240;240m %s \033[0;2;3m %s\n\n' "${1-}" "${*:2}"
@@ -150,6 +145,7 @@ function print_step_complete() {
 
 # install homebrew (if needed) and some commonly-used global packages. the bare minimum.
 function setup_brew() {
+  local BREWFILE
 
   print_banner step $'Installing/upgrading \033[1;4;35mhomebrew\033[0;1m and required formulae...'
   {
@@ -157,81 +153,42 @@ function setup_brew() {
     if ! command -v brew &> /dev/null; then
       curl -fsSL https://raw.github.com/Homebrew/install/HEAD/install.sh | bash -
     fi
-
     # execute now just to be sure its available for us immediately
     eval "$(brew shellenv 2> /dev/null)"
-
-    brew install "${verbosity-}" --overwrite rsync coreutils gh git-extras
-    # brew reinstall "${verbosity-}" coreutils starship gh shfmt;
   } | tee -i -a "$DOTFILES_LOG" 2>&1
+
+  # set our brewfile location
+  BREWFILE="$(realpath -eLq "${DOTFILES_PREFIX-}/.Brewfile" 2> /dev/null || echo -n "$DOTFILES_PREFIX/.Brewfile")"
 
   # don't install when we're in a noninteractive environment (read: gitpod dotfiles setup task)
   # otherwise this usually takes > 120s and fails hard thanks to gitpod's rather unreasonable time limit.
   # also, skip this step if we're in CI/CD (github actions), because... money reasons.
   if [ -z "${CI:+x}" ] && [ -n "${IS_INTERACTIVE+x}" ]; then
-    read -r -n 1 -i y -t 30 -p $'\n\033[0;1;5;33m⚠  \033[0m \033[1;33mInstall/update core utilities like git, go, docker, bash, etc.?\033[0m\n\n\033[0;2m(might add 5+ minutes to install)\n\n\033[0;2m(\033[0;1;32mY\033[0;1;2;32mes\033[0;3m / \033[0;1;31mN\033[0;1;2;31mo\033[0;2m)\033[0m ... '
-    local prompt_status=$?
-    echo ''
 
-    if [[ $REPLY =~ ^[Yy]$ ]] || ((prompt_status > 128)); then
-      # install some essentials
-      {
-        brew install "${verbosity-}" --overwrite \
-          gcc \
-          cmake \
-          make \
-          bash \
-          git \
-          go \
-          jq \
-          docker \
-          fzf \
-          neovim \
-          lolcat \
-          shellcheck \
-          shfmt \
-          pygments \
-          supabase/tap/supabase
-
-        # for developing jekyll sites (and other things for github pages, etc)
-        if command -v gem &> /dev/null; then
-          gem install jekyll bundler 2> /dev/null
-        fi
-      } | tee -i -a "$DOTFILES_LOG" 2>&1
+    # for developing jekyll sites (and other things for github pages, etc)
+    if command -v gem &> /dev/null; then
+      gem install jekyll bundler >> "$DOTFILES_LOG" 2>&1
     fi
 
     # for macOS
     if [ "$IS_DARWIN" = 1 ]; then
+      BREWFILE="$(realpath -eLq "${DOTFILES_PREFIX-}/.osx/.Brewfile" 2> /dev/null || echo -n "$DOTFILES_PREFIX/.osx/.Brewfile")"
       {
         command -v rvm &> /dev/null || curl -fsSL https://get.rvm.io | bash -s stable --rails
-
-        brew tap jeroenknoops/tap
-        brew install "${verbosity-}" gcc coreutils gitin gpg gpg-suite pinentry-mac
         export SHELL="${HOMEBREW_PREFIX:-}/bin/bash"
 
-        # if interactive and on macOS, we're probably on a macbook / iMac desktop. so add casks. (apps)
-        read -r -n 1 -i y -t 30 -p $'\n\033[0;1;5;33m⚠  \033[0m \033[1;33mInstall or upgrade \033[3;4mall\033[0;1;33m macOS apps, addons, and fonts?\033[0m\n\n\033[0;2m(could take up to ten minutes to complete)\n\n\033[0;2m(\033[0;1;32mY\033[0;1;2;32mes\033[0;2m / \033[0;1;31mN\033[0;1;2;31mo\033[0;2m)\033[0m ... '
-        local prompt_status=$?
-        echo ''
-
-        if [[ $REPLY =~ ^[Yy]$ ]] || ((prompt_status > 128)); then
-            brew install --quiet python3 fontforge
-            brew install --quiet --cask iterm2
-            curl -fsSL https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash -
-            # Google Chrome, Prisma, PIA VPN
-            brew tap homebrew/cask-versions
-            brew tap homebrew/cask-fonts
-            brew install --casks font-fira-code font-oswald font-ubuntu font-caskaydia-cove-nerd-font fontforge \
-                graphql-playground prisma-studio private-internet-access qlmarkdown \
-                visual-studio-code visual-studio-code-insiders \
-                google-chrome google-chrome-canary firefox firefox-nightly
-        fi
       } | tee -i -a "$DOTFILES_LOG" 2>&1
-    else
-      brew install --quiet --overwrite gnupg2 xclip
     fi
   fi
 
+  if [ -r "$BREWFILE" ]; then
+    # make sure its executable...
+    [ -x "$BREWFILE" ] || chmod +x "$BREWFILE" 2>/dev/null;
+    # and then execute it
+    brew bundle install "${verbosity-}" --overwrite --display-times --file="${BREWFILE:-"$HOME/.Brewfile"}" --no-lock | tee -i -a "$DOTFILES_LOG" 2>&1;
+  fi
+
+  # shellcheck disable=SC2119
   print_step_complete
 }
 
@@ -241,7 +198,10 @@ function setup_node() {
   local node_v
   node_v="${1:-lts}"
 
-  print_banner step $'Installing/upgrading \033[1;4;31mPNPM\033[0;1m and \033[4;32mNode\033[0;1;2m v'"$node_v"$'\033[0m'
+  local -a global_util_packages=(zx fsxx @brlt/n @brlt/prettier prettier @brlt/eslint-config eslint @brlt/utils degit)
+  local -a global_cli_packages=(dotenv-vault vercel wrangler@latest miniflare@latest @railway/cli netlify-cli)
+
+  print_banner step "$(printf 'Installing/upgrading \033[1;4;31mPNPM\033[0;1m and \033[4;32mNode\033[0;1;2m v%s\033[0m' "${node_v:-LTS}")"
 
   {
     # install pnpm if not already installed
@@ -251,45 +211,69 @@ function setup_node() {
     # ensure we have node.js installed
     if ! command -v node &> /dev/null || ! node -v &> /dev/null; then
       {
-        pnpm env use -g "${node_v:-lts}" 2> /dev/null || pnpm env use -g 16.15.0
+        pnpm env use -g "${node_v:-lts}" 2> /dev/null || pnpm env use -g 16
       } && pnpm setup 2> /dev/null
     fi
   } | tee -i -a "$DOTFILES_LOG" 2>&1
 
-  global_add zx @brlt/n @brlt/prettier prettier @brlt/eslint-config eslint @brlt/utils degit
+  global_add "${global_util_packages[@]}"
 
-  read -r -n 1 -i y -t 30 -p $'\n\033[0;1;5;33m⚠  \033[0m\033[1;33mInstall CLIs for Vercel/Railway/Netlify/Cloudflare?\033[0m\n\n\033[0;2m(\033[0;1;4;32mY\033[0;1;2;32mes\033[0;2m / \033[0;1;4;31mN\033[0;1;2;31mo\033[0;2m)\033[0m ... '
-  local prompt_status=$?
-  echo ''
+  if [ -z "${IS_INTERACTIVE+x}" ]; then
+    global_add "${global_cli_packages[@]}"
+  else # interactive
+    read -r -n 1 -i y -t 30 -p $'\n\033[0;1;5;33m⚠  \033[0m\033[1;33mInstall CLIs for Vercel/Railway/Netlify/Cloudflare?\033[0m\n\n\033[0;2m(\033[0;1;4;32mY\033[0;1;2;32mes\033[0;2m / \033[0;1;4;31mN\033[0;1;2;31mo\033[0;2m)\033[0m ... '
+    local prompt_status=$?
+    echo ''
 
-  if [[ $REPLY =~ ^[Yy]$ ]] || (("$prompt_status" > 128)); then
-    global_add dotenv-vault vercel wrangler@latest miniflare@latest @railway/cli netlify-cli
-  else
-    STEP_NUM=$((STEP_NUM + 1))
+    if [[ $REPLY == [Yy]* ]] || ((prompt_status > 128)); then
+      global_add "${global_cli_packages[@]-}"
+    fi
+
+    STEP_NUM=$((STEP_NUM+1))
   fi
 
-  print_step_complete "$@"
+  # shellcheck disable=SC2119
+  print_step_complete
 }
 
 # setup our new homedir with symlinks to all the dotfiles
 function setup_home() {
+  local rsyncfiles rsyncexclude rsyncinclude git_file
+  local -a rsyncargs=()
+
   # backup (preserving homedir structure), e.g. /home/gitpod/.dotfiles/.backup/home/gitpod/.bashrc~
   [ -d "${DOTFILES_BACKUP_PATH-}" ] || mkdir -p "${DOTFILES_BACKUP_PATH-}" &> /dev/null
 
-  # this part used to use (and most other dotfiles projects still do) symlinks/hardlinks between
-  # the ~/.dotfiles folder and the homedir, but it now uses the rsync program for configuring the
-  # homedir. while it is an external dependency, it is much more performant, reliable, portable,
-  # and maintainable. +/- ~100 lines of code were replaced by 1 line... and it creates backups ;)
+  # set the default locations for .rsyncfiles and .rsyncexclude files
+  rsyncfiles="${DOTFILES_PREFIX-}/.rsyncfiles"
+  rsyncexclude="${DOTFILES_PREFIX-}/.rsyncexclude"
+  rsyncinclude="${DOTFILES_PREFIX-}/.rsyncinclude"
 
-  # define exclusions with .rsyncignore; define files to copy with .rsyncinclude file
-  rsync -avh --backup --backup-dir="${DOTFILES_BACKUP_PATH-}" --whole-file --files-from=.rsyncfiles --exclude-from=.rsyncignore . ~ | tee -i -a "$DOTFILES_LOG" 2>&1
+  # set the common arguments used by all of our rsync invocations
+  rsyncargs+=(-avh --recursive --perms --times --checksum --itemize-changes --human-readable --progress --log-file="${DOTFILES_LOG}" --backup --backup-dir="${DOTFILES_BACKUP_PATH-}")
 
   # .gitignore and .gitconfig are special!
   # we have to rename them to avoid issues with git applying them to the repository
-  mv -f ~/.{gitignore,gitconfig} "${DOTFILES_BACKUP_PATH-}" &> /dev/null
+  for git_file in "gitignore" "gitconfig"; do
+    mv -f -v ~/."$git_file" "${DOTFILES_BACKUP_PATH-}" | tee -i -a "$DOTFILES_LOG" 2>&1
+    rsync "${rsyncargs[@]}" "$git_file" ~/."$git_file";
+  done
 
-  rsync -avh --backup --backup-dir="${DOTFILES_BACKUP_PATH-}" --whole-file gitignore ~/.gitignore | tee -i -a "$DOTFILES_LOG" 2> /dev/null
-  rsync -avh --backup --backup-dir="${DOTFILES_BACKUP_PATH-}" --whole-file gitconfig ~/.gitconfig | tee -i -a "$DOTFILES_LOG" 2> /dev/null
+
+  # exclude files matching glob patterns in the file .dotfiles/.rsyncexclude
+  [ -n "${rsyncexclude-}" ] && [ -r "${rsyncexclude-}" ] &&
+    rsyncargs+=(--exclude-from="${rsyncexclude-}");
+
+  # include files with glob patterns in the file .dotfiles/.rsyncinclude
+  [ -n "${rsyncinclude-}" ] && [ -r "${rsyncinclude-}" ] &&
+    rsyncargs+=(--include-from="${rsyncinclude-}");
+
+  # explicitly list files to copy in .dotfiles/.rsyncfiles
+  [ -n "${rsyncfiles-}" ] && [ -r "${rsyncfiles-}" ] &&
+    rsyncargs+=(--files-from="${rsyncfiles-}");
+
+  # now do the damn thang!
+  rsync "${rsyncargs[@]}" "$(curdir 2>/dev/null || echo -n "$DOTFILES_PREFIX")" "$HOME";
 
   return 0
 }
@@ -297,7 +281,7 @@ function setup_home() {
 # runs all the other scripts and cleans up after itself. geronimo!
 function main() {
   # make sure we are in the root ~/.dotfiles directory
-  cd "${DOTFILES_PREFIX:-$HOME/.dotfiles}" || return
+  cd "${DOTFILES_PREFIX-}" || return ;
 
   print_banner "Spooling up the dotfiles installer..."
   printf ' \033[1;4m%s\033[0m: %s \n\n' 'Working Dir' "$(curdir)"
@@ -306,25 +290,21 @@ function main() {
   # currently the homebrew installer breaks due to a git syntax error in their code. works fine in gitpod though. 🤔
   if [ -n "${CODESPACES+x}" ]; then
     STEP_TOTAL=2 # adjust step total since we're skipping homebrew
-    {
-      curl -sS https://starship.rs/install.sh | sh -
-    }
+    curl -sS https://starship.rs/install.sh | sh -
   else
     STEP_TOTAL=3
-    # for everything else, setup homebrew and install some packages / formulae
-    setup_brew
-    brew install --quiet --overwrite starship
+    setup_brew # for everything else
   fi
 
   # setup pnpm + node.js and install some global packages I frequently use
   # pin node.js to 16.14.2 to prevent breaking errors
-  setup_node "16.15.0"
+  setup_node "16.15.0";
 
   # syncing the home directory
   print_banner step $'Syncing \033[1;4;33mdotfiles\033[0;1m to \033[1;3;4;36m'"$HOME"
 
   # if we are in interactive mode (not in CI/CD), ask the user if they want to proceed
-  if is_interactive 2> /dev/null; then
+  if is_interactive 2>/dev/null || [ -n "${IS_INTERACTIVE+x}" ]; then
     read -r -n 1 -i y -t 30 -p $'\n\033[0;1;5;33m⚠  \033[0;1;31m DANGER \033[0m ·  \033[3;31mContinuing with install will overwrite existing files in \033[3;4m'"$HOME"$'\033[0;3;31m.\033[0m\n\n\033[0;1;4;33mAccept and continue?\033[0;2m (respond within 30s or \033[1m"Yes"\033[0;2m is assumed)\n\n\033[0;2m(\033[0;1;4;32mY\033[0;1;2;32mes\033[0;2m / \033[0;1;4;31mN\033[0;1;2;31mo\033[0;2m)\033[0m ... '
     # if the user says yes, or force, run the install
     if (($? > 128)) || [[ $REPLY == [Yy]* ]]; then
@@ -351,15 +331,14 @@ function main() {
 }
 
 function cleanup_env() {
-  unset -v STEP_NUM STEP_TOTAL verbosity IS_INTERACTIVE IS_DARWIN cmd_alias
-  unset -f main setup_home setup_node setup_brew print_step_complete print_banner global_add is_interactive setup_env
+  unset -v STEP_NUM STEP_TOTAL verbosity IS_INTERACTIVE IS_DARWIN cmd_alias 2>/dev/null
+  unset -f main setup_home setup_node setup_brew print_step_complete print_banner global_add is_interactive 2>/dev/null
 }
 
-# run it and clean up after!
+# run dat shizzle
 {
-  setup_env 2> /dev/null
-  main "$@" || exit $?
-} | tee -i -a "$DOTFILES_LOG" 2>&1
+  main "$@" | tee -i -a "$DOTFILES_LOG" 2>&1; # pipe the logs to "$DOTFILES_LOG"
+} || code=$?;
 
-cleanup_env && unset -f cleanup_env
-exit 0
+# shellcheck disable=SC2248
+cleanup_env && unset -f cleanup_env && exit ${code:-0};
